f476204daf22b0851648e11bd271d61f8855ff02
sjy366 <sjy20131565@gmail.com>
2018-08-25 00:56:36 +0900

    서버에서 이미지를 받는 기능 완성

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+var request = require('request');
+
+app.use(express.json());
+
+var download = function(uri, filename, callback){
+  request.head(uri, function(err, res, body){
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
 
 app.get('/keyboard', function(req, res){
     var keySetting = {
@@ -7,6 +20,14 @@ app.get('/keyboard', function(req, res){
     }
     res.send(JSON.stringify(keySetting));
 });
+
+app.post('/message', function(req, res){
+    console.log(req.body);
+    download(req.body.content, 'output/test.jpg', function(){
+        console.log('image downloading complete!')
+    });
+});
+
 app.listen(8080, function(){
     console.log('server is running...');
 });
