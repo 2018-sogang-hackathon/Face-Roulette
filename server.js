9bbdfb52fc1e4b767887fc9ae398ebae46bd576d
sjy366 <sjy20131565@gmail.com>
2018-08-25 02:43:26 +0900

    프로토타입 완성

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var request = require('request');
+var img_proc = require('./image_process.js');
 
 app.use(express.json());
 var host_url = 'http://2a771f84.ngrok.io';
@@ -23,20 +24,23 @@ app.get('/keyboard', function(req, res){
 });
 
 app.post('/message', function(req, res){
-    console.log(req.body);
-    download(req.body.content, 'output/test.jpg', function(){
-        console.log('image downloading complete!')
+    console.log('image downloading complete!')
+    var prom = img_proc.imageProcess(req.body.content);
+    prom.then(function(num_of_people){
+        var pick_num = Math.floor(Math.random() * (0, num_of_people) + 0);
         var resSetting = {
             "message": {
                 "photo": {
-                    "url": host_url + "/input/test.jpg",
+                    "url": host_url + "/output/" + pick_num.toString() + ".jpg",
                     "width": 640,
                     "height": 480
                 }
             }
         };
+        console.log(pick_num);
+        console.log(num_of_people);
         res.send(JSON.stringify(resSetting));
-    });
+    })
 });
 
 app.get('*', function(req, res){
