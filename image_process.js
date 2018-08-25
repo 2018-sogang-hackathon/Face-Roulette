4bfaa4cced3451dc125c6c47e85ec2c8df1d2ce0
KiyoungYoon <craclog@gmail.com>
2018-08-25 11:26:21 +0900

    Fix cropping

--- image_process.js
+++ image_process.js
@@ -41,39 +41,57 @@ function imgProcess(imageUrl, userID) {
                 console.log('length is zero');
                 resolve(0);
             }
+            Jimp.read(imageUrl).then(image =>{
+                // Get width, height of original image.
+                var w = image.bitmap.width;
+                var h = image.bitmap.height;
+                return {width: w, height: h};
+            }).then(obj => {
+                var origin_width = obj.width;
+                var origin_height = obj.height;
+                var alpha = 0.5;
+                //console.log(origin_width, origin_height);
+                for (var i = 0; i < jsonResponse.length; i++) {
+                    var top = jsonResponse[i].faceRectangle.top;
+                    var left = jsonResponse[i].faceRectangle.left;
+                    var width = jsonResponse[i].faceRectangle.width;
+                    var height = jsonResponse[i].faceRectangle.height;
 
-            for (var i = 0; i < jsonResponse.length; i++) {
-                var top = jsonResponse[i].faceRectangle.top - 30;
-                var left = jsonResponse[i].faceRectangle.left - 30;
-                var width = jsonResponse[i].faceRectangle.width + 60;
-                var height = jsonResponse[i].faceRectangle.height + 60;
-    
-                Jimp.read(imageUrl, ((i, top, left, width, height) => (err, lenna) => {
-                    if (err) throw err;
-                    lenna
-                        .crop(left, top, width, height)
-                        .writeAsync(output_path + userID + '_' + i.toString() + '.jpg').then(function(){
-                            count++;
-                            if(count == jsonResponse.length){
-                                var pick_number = Math.floor(Math.random() * (count - 0) + 0);
-                                var ret = {
-                                    "pick_number": pick_number,
-                                    "width": jsonResponse[pick_number].faceRectangle.width + 60,
-                                    "height": jsonResponse[pick_number].faceRectangle.height + 60,
-                                    "num_of_people": count
-                                };
-                                resolve(ret);
-                            }
-                        });
-                    console.log('img created');
-                })(i, top, left, width, height));
-            }
+                    var gap = left - Math.max(left - width*alpha, 0);
+                    var gap2 = Math.min(width*(1+2*alpha), origin_width) - (left + width);
+                    left -= Math.min(gap, gap2);
+                    width += Math.min(gap, gap2) * 2;
+                    var gap = top - Math.max(top - height*alpha, 0);
+                    var gap2 = Math.min(height*(1+2*alpha), origin_height) - (top + height);
+                    top -= Math.min(gap, gap2);
+                    height += Math.min(gap, gap2) * 2;
+
+                    Jimp.read(imageUrl, ((i, top, left, width, height) => (err, lenna) => {
+                        if (err) throw err;
+                        lenna
+                            .crop(left, top, width, height)
+                            .writeAsync(output_path + userID + '_' + i.toString() + '.jpg').then(function(){
+                                count++;
+                                if(count == jsonResponse.length){
+                                    var pick_number = Math.floor(Math.random() * (count - 0) + 0);
+                                    var ret = {
+                                        "pick_number": pick_number,
+                                        "width": jsonResponse[pick_number].faceRectangle.width + 60,
+                                        "height": jsonResponse[pick_number].faceRectangle.height + 60,
+                                        "num_of_people": count
+                                    };
+                                    resolve(ret);
+                                }
+                            });
+                    })(i, top, left, width, height));
+                }
+            })
+
         });
     });
 };
-// Test Code
-var testURL = 'https://img.huffingtonpost.com/asset/5ab1b7562000007d06eb27f0.jpeg?ops=scalefit_630_noupscale';
-var testID = 'qwer';
-imgProcess(testURL, testID);
-//console.log(imgProcess);
-//asdf
+// // Test Code
+// var testURL = 'http://dn-m.talk.kakao.com/talkm/bl2TiOCu5js/xh4Oqs5ClcWqjHfLMfJdl1/i_soqvnbua000h1.jpg'; //현석얼굴
+// //var testURL = 'https://img.huffingtonpost.com/asset/5ab1b7562000007d06eb27f0.jpeg?ops=scalefit_630_noupscale'; // 레드벨벳5인
+// var testID = '05width+height3';
+// imgProcess(testURL, testID);
