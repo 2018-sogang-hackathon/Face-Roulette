fa40d8c8803521098d851f16e3eb437a4e81f0e0
JinwookMBP <vustthat@gmail.com>
2018-08-26 12:49:39 +0900

    (리얼서버 테스트 요함) 공유페이지 템플릿

---
+++ client.js
@@ -0,0 +1,41 @@
+/* eslint-env browser */
+
+var getParameters = function (paramName) {
+  // 리턴값을 위한 변수 선언
+  var returnValue;
+
+  // 현재 URL 가져오기
+  var url = location.href;
+
+  // get 파라미터 값을 가져올 수 있는 ? 를 기점으로 slice 한 후 split 으로 나눔
+  var parameters = (url.slice(url.indexOf('?') + 1, url.length)).split('&');
+
+  // 나누어진 값의 비교를 통해 paramName 으로 요청된 데이터의 값만 return
+  for (var i = 0; i < parameters.length; i++) {
+    var varName = parameters[i].split('=')[0];
+    if (varName.toUpperCase() == paramName.toUpperCase()) {
+      returnValue = parameters[i].split('=')[1];
+      return decodeURIComponent(returnValue);
+    }
+  }
+};
+
+/////////////////////////////////////
+const REMOTE_DIR = 'https://ec2-52-79-228-242.ap-northeast-2.compute.amazonaws.com/output'
+
+var img_id = getParameters('img_id');
+var img_picked = getParameters('img_picked');
+
+var croppedUrl = `${REMOTE_DIR}/${img_id}_${img_picked}.jpg`;
+var sourceUrl = decodeURIComponent(getParameters('original'));
+var title = decodeURIComponent(getParameters('title'));
+
+// croppedUrl = './cropped-example.jpg';
+// sourceUrl = './source-example.jpg';
+
+document.querySelector('#source_img').src = sourceUrl;
+document.querySelector('#cropped_img').src = croppedUrl;
+document.querySelector('.message > span').textContent = title;
+
+// orignal : 카톡 서버에 있는 원본 사진 url
+// https://ec2-52-79-228-242.ap-northeast-2.compute.amazonaws.com/output/img_id + '_' + 'img_picked' : 크롭된(picked) 사진 url
